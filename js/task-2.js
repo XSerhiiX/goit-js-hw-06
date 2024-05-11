71dcf880cf5a597ca726e0be009ef671b97f89c7
XSerhiiX <avramserg1987@gmail.com>
2024-05-11 22:07:21 +0300

    Update task-2.js

--- js/task-2.js
+++ js/task-2.js
@@ -6,34 +6,30 @@
 // addItem(newItem) — приймає новий товар newItem і додає його до масиву товарів у приватну властивість items об'єкта.
 // removeItem(itemToRemove) — приймає рядок з назвою товару itemToRemove і видаляє його з масиву товарів у приватній властивості items об'єкта.
 
-
 class Storage {
-  #items = [];
-
-  constructor(items) {
+  #items;
+  constructor([...items]) {
     this.#items = items;
   }
-
   getItems() {
     return this.#items;
   }
-
   addItem(newItem) {
     this.#items.push(newItem);
   }
-
   removeItem(itemToRemove) {
-    const index = this.#items.indexOf(itemToRemove);
-    if (index !== -1) {
-      this.#items.splice(index, 1);
-    }
+    this.#items = this.#items.filter((item) => itemToRemove !== item);
   }
 }
 
 const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
 console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+
 storage.addItem("Droid");
 console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+
 storage.removeItem("Prolonger");
 console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
 
+storage.removeItem("Scaner");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
